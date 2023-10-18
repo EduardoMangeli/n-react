33394aafddf04943ef81d5083a79583659d8262b
EduardoMangeli <eduardo@mangeli.com.br>
2023-10-18 11:16:37 -0300

    styled component

--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,14 +1,28 @@
-import LinkGenerico from "../components/LinkGenerico";
+import LinkGenerico from "../components/LinkGenerico/LinkGenerico";
 import LinkSegundo from "../components/LinkSegundo";
 
+const dados = [
+    ["https://www.ibmec.br/", "ibmec"],
+    ["https://estude.ibmec.br/ibmec-hubs", "ibmec-hubs"],
+    ["https://botafogo.com.br/", "botafogo"]
+];
+
+const elementos = dados.map(
+    (item, i) => (
+        <LinkGenerico
+                key={i} 
+                texto={item[1]}
+                endereco={item[0]}
+        />
+    )
+);
+
 const Primeiro = () => {
+    
     return (<div>
                 <h5>Mini título</h5>
                 <p>Meu texto</p>
-                <LinkGenerico 
-                    texto="Link Genérico"
-                    endereco="segunda"
-                />
+                {elementos}
             </div>
             );
 };
