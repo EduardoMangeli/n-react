64eb059cde84c3d14cd8f275f90fbc4d4dc0dcc3
EduardoMangeli <eduardo@mangeli.com.br>
2023-10-18 10:43:15 -0300

    link generico

--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,10 +1,14 @@
+import LinkGenerico from "../components/LinkGenerico";
 import LinkSegundo from "../components/LinkSegundo";
 
 const Primeiro = () => {
     return (<div>
                 <h5>Mini título</h5>
                 <p>Meu texto</p>
-                <LinkSegundo />
+                <LinkGenerico 
+                    texto="Link Genérico"
+                    endereco="segunda"
+                />
             </div>
             );
 };
